8455fdf08714833f6311b5fc0b671d116fc67147
Rehman Ali <112756932+Rehmanali03@users.noreply.github.com>
2023-04-11 13:39:01 +0500

    initial commit
    in this file i have loop if else and switch case.

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a , b;
+            Console.WriteLine("Enter Number One");
+            a = Convert.ToInt32( Console.ReadLine());
+
+            string op;
+            Console.WriteLine("Enter Oprator");
+            op = Console.ReadLine();
+
+            Console.WriteLine("Enter Second Number");
+            b = Convert.ToInt32( Console.ReadLine());
+
+            switch (op)
+            {
+                case "+": 
+                    Console.WriteLine(a + b);
+                break;
+
+                case "-":
+                    Console.WriteLine(a - b);
+                break;
+
+                case "*":
+                    Console.WriteLine(a * b);
+                break;
+
+                case "/":
+                    Console.WriteLine(a / b);
+                break;
+
+               default:
+                    Console.WriteLine("Enter Valid Oprator");
+               break;
+
+            }
+
+            Console.Read();
+        }
+    }
+}
+//int i;
+//for (i = 0; i <= 10; i++)
+//{
+//    Console.WriteLine("{0}*{1}={2}", a, i, a * i);
+//}
+
+
+//if(op == "+")
+//{
+//    Console.WriteLine(a + b);
+
+//}
+//  else if(op == "*")
+//{
+//    Console.WriteLine(a * b);
+//}
+
+//else if (op == "-")
+//{
+//    Console.WriteLine(a - b);
+//}
+//else if (op == "/")
+//{
+//    Console.WriteLine(a / b);
+//}
+//else
+//{
+//    Console.WriteLine("Enter Valid Oprator");
+//}
